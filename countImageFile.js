1e6f612c4fe48faa31834c53efa6d867addf1c02
chiehan <chieh.an.hsu@gmail.com>
2017-04-18 17:48:19 +0800

    update

--- countImageFile.js
+++ countImageFile.js
@@ -3,16 +3,35 @@ var fs = require('fs');
 var naturalSort = require('javascript-natural-sort');
 var Path = require('path');
 
-var repos = ['jiangkangyur', 'degekangyur', 'degetengyur', 'mipam', 'gorampa', 'gampopa', '8thkarmapa', 'tsongkhapa'];
-var repoImages = repos.map(countRepoImage);
-showRepoImage(repoImages);
+//var repos = ['jiangkangyur', 'degekangyur', 'degetengyur', 'mipam', 'gorampa', 'gampopa', '8thkarmapa', 'tsongkhapa'];
+var repos = ['gampopa'];
+var allImages = repos.map(countRepoImage);
+showRepoImage(allImages);
 
-function showRepoImage(repoImages) {
-  fs.writeFileSync('./result.txt', JSON.stringify(repoImages, null, '  '), 'utf8');
+function showRepoImage(allImages) {
+  var text = [], appendix = [];
+
+  allImages.forEach(function(repo) {
+    var folders = repo.folders;
+
+    text.push(repo.repoName);
+    text.push('  總計 ' + repo.folderN + ' 資料夾，' + repo.totalImageN + ' 個圖檔。');
+
+    appendix.push(repoName + '圖檔明細');
+    folders.forEach(function (folder) {
+      for (var folderName in folder) {
+        var imageN = String(folder[folderName]);
+        appendix.push('  ' + folderName + ' 資料夾，總計 '+ imageN + ' 個圖檔');
+      }
+    });
+  });
+
+  fs.writeFileSync('./result.txt', text.join('\n'), 'utf8');
 }
 
 function countRepoImage(repo) {
   var result = {repoName: repo, folderN: 0};
+  var folders = {};
 
   var routes = glob.sync('../../totalImages/' + repo + '/' + repo + '*/*.jpg')
     .sort(naturalSort);
@@ -20,14 +39,17 @@ function countRepoImage(repo) {
   result.totalImageN = routes.length;
 
   routes.forEach(function(route) {
-    var folder = /\/([^/]+?)$/.exec(Path.dirname(route))[1];
-    if (! result[folder]) {
-      result[folder] = 1;
+    var folderName = /\/([^/]+?)$/.exec(Path.dirname(route))[1];
+    if (! folders[folderName]) {
+      folders[folderName] = 1;
       result.folderN++;
     }
     else {
-      result[folder]++;
+      folders[folderName]++;
     }
   });
+
+  result.folders = folders;
+
   return result;
 }
